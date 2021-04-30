eed1e5090b8272e686a7e8eac43a1d489f3d3fdb
Martin Szyska <martinszyska37@gmail.com>
2021-04-30 10:20:58 +0200

    collapse zeros for both ponsize and ponsize-1 for reduced matrice computations due to tumor matching PON

--- scripts/EBcache_zeros.py
+++ scripts/EBcache_zeros.py
@@ -5,6 +5,7 @@ import pandas as pd
 # add ebscore package to sys.path
 sys.path.append(os.path.join(snakemake.scriptdir, "ebscore/code"))
 from zerocache import collapse_zeros
+from script_utils import show_output
 
 
 def main(s):
@@ -19,14 +20,17 @@ def main(s):
     # get the pon_size from the PONlist
 
     pon_file = os.path.join(p.pon_path, p.pon_list)
-    pon_size = len(pd.read_csv(pon_file, sep="\t", header=None).index)
+    ponsize = len(pd.read_csv(pon_file, sep="\t", header=None).index)
+    zero_path = os.path.join(p.pon_path, cc["zero_path"])
 
-    collapse_zeros(
-        os.path.join(p.pon_path, "zero"),
-        pon_size=pon_size,
-        zero_condense_factor=cc['params']['zero_condense_factor'],
-        reflat=p.reflat
-    )
+    for ps in [ponsize, ponsize - 1]:
+        show_output(f"Collapsing zeros for in {zero_path} for ponsize {ps}.")
+        collapse_zeros(
+            zero_path,
+            ponsize=ps,
+            ZDfactor=cc["params"]["ZDfactor"],
+            reflat=p.reflat,
+        )
 
 
 if __name__ == "__main__":
