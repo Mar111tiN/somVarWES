0689af75a0f932464ff26ad129f0c28a9fa95452
Martin Szyska <martinszyska37@gmail.com>
2023-04-04 22:45:45 +0200

    fix missing ChrStartRef for AB matrix

--- cacheSLURM.sh
+++ cacheSLURM.sh
@@ -31,6 +31,7 @@ echo $CONDA_PREFIX "activated";
 SLURM_CLUSTER="sbatch -p {cluster.partition} -t {cluster.t} --mem-per-cpu={cluster.mem} -J {cluster.name} --nodes={cluster.nodes} -n {cluster.threads}";
 SLURM_CLUSTER="$SLURM_CLUSTER -o ${LOGDIR}/{rule}-%j.log";
 snakemake --snakefile cacheSnakefile --unlock
+snakemake --dag | awk '$0 ~ "digraph" {p=1} p' | dot -Tsvg > dax/cacheDag.svg
 snakemake --snakefile cacheSnakefile --use-conda \
 --rerun-incomplete \
 --cluster-config configs/cluster/PONcache-cluster.json \
